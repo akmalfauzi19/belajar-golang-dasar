04c3798910646b7a3705923fa26321ab217e5ee5
Hafizh Akmal Fauzi <akmal.fauzi19@gmail.com>
2024-11-23 21:57:37 +0700

    add private and public functions in helper package

--- helper/helper.go
+++ helper/helper.go
@@ -1,5 +1,20 @@
 package helper // sesuai dengan nama folder
 
+var Application = "Golang"
+var version = 1.0
+
+// fungsi private
+func sayGoodBye(Name string) string {
+	return "Good Bye " + Name
+}
+
+// fungsi public
 func SayHello(Name string) string {
 	return "Hello " + Name
 }
+
+//  penjelasan
+/**
+- fungsi sayGoodBye merupakan fungsi private, karena diawali dengan huruf kecil
+- fungsi SayHello merupakan fungsi public, karena diawali dengan huruf besar
+**/
--- import.go
+++ import.go
@@ -8,4 +8,10 @@ import (
 func main() {
 	result := helper.SayHello("Akmal")
 	fmt.Println(result)
+
+	fmt.Println(helper.Application)
+	// fmt.Println(helper.version) // error
+	// fmt.Println(helper.sayGoodBye("Akmal")) // error
+
+	// jika ingin mengakses fungsi private, maka harus membuat fungsi public yang mengakses fungsi private tersebut
 }
